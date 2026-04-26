0e4491f68738c018b1ce6607c5276c040a9541e2
Light <lightman.chang@gmail.com>
2026-04-26 11:08:19 +0800

    feat: add Playwright fetchers for all tracked companies

--- fetchers/__init__.py
+++ fetchers/__init__.py
@@ -1,6 +1,35 @@
 """
-Company fetcher registry.
-Add Playwright-based fetchers here as they are implemented.
+公司新聞爬蟲
 """
 
-FETCHERS = {}
+from .base import CompanyFetcher, CompanyDocument
+
+from .abtc import AbtcFetcher
+from .aurubis import AurubisFetcher
+from .boliden import BolidenFetcher
+from .glencore import GlencoreFetcher
+from .kunding import KundingFetcher
+from .renewi import RenewiFetcher
+from .republic import RepublicFetcher
+from .sims import SimsFetcher
+from .tomra import TomraFetcher
+from .umicore import UmicoreFetcher
+from .veolia import VeoliaFetcher
+from .waste_connections import WasteConnectionsFetcher
+from .waste_mgmt import WasteMgmtFetcher
+
+FETCHERS = {
+    "abtc": AbtcFetcher,
+    "aurubis": AurubisFetcher,
+    "boliden": BolidenFetcher,
+    "glencore": GlencoreFetcher,
+    "kunding": KundingFetcher,
+    "renewi": RenewiFetcher,
+    "republic": RepublicFetcher,
+    "sims": SimsFetcher,
+    "tomra": TomraFetcher,
+    "umicore": UmicoreFetcher,
+    "veolia": VeoliaFetcher,
+    "waste_connections": WasteConnectionsFetcher,
+    "waste_mgmt": WasteMgmtFetcher,
+}
